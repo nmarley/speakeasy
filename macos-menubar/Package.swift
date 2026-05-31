03926a7087b13f8b8dd624bd72dcd389f676c99e
Nathan Marley <nathan@glowberrylabs.com>
2026-05-30 17:23:30 -0700

    Bump deployment target to macOS 13
    SMAppService requires macOS 13 Ventura. The previous target of macOS 12
    Monterey has been EOL since late 2024. Also fixes the stale
    LSMinimumSystemVersion in Info.plist, which still referenced macOS 10.12.

--- macos-menubar/Package.swift
+++ macos-menubar/Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "Speakeasy",
     platforms: [
-        .macOS(.v12)
+        .macOS(.v13)
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
